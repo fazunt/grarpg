a126d26da8aaf5a880c6ff9cf13cd9a255d19ca0
fazunt <147977188+fazunt@users.noreply.github.com>
2024-01-10 13:52:15 +0100

    Add files via upload

---
+++ PD/main.py
@@ -0,0 +1,226 @@
+class Pierwiastek1:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek1("glin", "Al", "1,5", "26.98", "13")
+p1.inf()
+
+
+class Pierwiastek2:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek2("krzem", "Si", "1,8", "28.09", "14")
+p1.inf()
+
+class Pierwiastek3:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek3("fosfor", "P", "2,1", "30.97", "15")
+p1.inf()
+
+class Pierwiastek4:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek4("siarka", "S", "2,5", "32.06", "16")
+p1.inf()
+
+class Pierwiastek5:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek5("chlor", "Cl", "3,0", "35.45", "17")
+p1.inf()
+
+class Pierwiastek6:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek6("argon", "Ar", "-", "39.95", "18")
+p1.inf()
+
+class Pierwiastek7:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek7("gal", "Ga", "1,6", "69.72", "31")
+p1.inf()
+
+class Pierwiastek8:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek8("german", "Ge", "1,8", "72.63", "32")
+p1.inf()
+
+class Pierwiastek9:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek9("arsen", "As", "2,0", "74.92", "33")
+p1.inf()
+
+class Pierwiastek10:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek10("selen", "Se", "2,4", "78.96", "34")
+p1.inf()
+
+class Pierwiastek11:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek11("brom", "Br", "2,8", "79.90", "35")
+p1.inf()
+
+class Pierwiastek12:
+    def __init__(self, _nazwa, _symbol, _elektro_ujemność, _masa_atomowa, _liczba_atomowa) -> None:
+        self.nazwa = _nazwa
+        self.symbol = _symbol
+        self.elektro_ujemność = _elektro_ujemność
+        self.masa_atomowa = _masa_atomowa
+        self.liczba_atomowa = _liczba_atomowa
+
+    def inf(self):
+        print("=======================")
+        print(f"nazwa {self.nazwa}")
+        print(f"symbol {self.symbol}")
+        print(f"elektro ujemność {self.elektro_ujemność}")
+        print(f"masa atommowa {self.masa_atomowa}")
+        print(f"liczba atomowa {self.liczba_atomowa}")
+
+p1 = Pierwiastek12("krypton", "Kr", "-", "83.80", "36")
